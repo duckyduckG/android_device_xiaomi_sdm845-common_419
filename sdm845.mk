9892eb00bdd7d498200e8a7039ad1a433dd93362
duckyduckG <duckyduckg65@gmail.com>
2025-08-09 20:19:27 +0500

    sdm845-common: switch to OSS sound trigger hal
    03-05 15:49:24.985   971  1008 I soundtrigger: audio_extn_sound_trigger_init: Enter
    03-05 15:49:24.985   971  1008 I soundtrigger: audio_extn_sound_trigger_init: DLOPEN successful for /vendor/lib/hw/sound_trigger.primary.sdm845.so
    03-05 15:49:24.985   971  1008 E soundtrigger: audio_extn_sound_trigger_init: Incompatible API versions ahal:0x200 != sthal:0x101

--- sdm845.mk
+++ sdm845.mk
@@ -99,6 +99,10 @@ PRODUCT_PACKAGES += \
     libvolumelistener \
     tinymix
 
+# OSS soundtrigger
+PRODUCT_PACKAGES += \
+    sound_trigger.primary.sdm845
+
 # Bluetooth
 PRODUCT_PACKAGES += \
     liba2dpoffload
