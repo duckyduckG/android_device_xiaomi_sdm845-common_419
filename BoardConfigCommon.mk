5d98d0e98b40d684d5c834da9e57f85b35a8a84e
duckyduckG <duckyduckg65@gmail.com>
2026-01-08 23:08:36 +0500

    sdm845-common: pick LXC config

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -46,7 +46,8 @@ BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_CONFIG := \
     vendor/sdm845-perf_defconfig \
-    vendor/xiaomi/sdm845-common.config
+    vendor/xiaomi/sdm845-common.config \
+    vendor/xiaomi/lxc.config
 
 TARGET_KERNEL_SOURCE := kernel/xiaomi/sdm845
 
